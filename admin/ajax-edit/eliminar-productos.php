5f69d3c1d25d8d4af763c128eec1f06ffb3d0d23
IssacPacheco <isaacpacheco.go@gmail.com>
2022-05-21 10:31:48 -0500

    Se modifica el eliminado

--- admin/ajax-edit/eliminar-productos.php
+++ admin/ajax-edit/eliminar-productos.php
@@ -3,15 +3,29 @@ include("../class/allClass.php");
 use conexionbd\mysqlconsultas;
 $ejecucion = new mysqlconsultas();
 
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-if(isset($id)){
+
+if(isset($_POST['eliminar'])){
+    $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
     $qry1 = "DELETE FROM inv_productos WHERE id = $id";
     $ejecucion->ejecuta($qry1);
 
     $qry2 = "DELETE FROM inv_campus_producto WHERE id_producto = $id AND id_campus = 1";
     $ejecucion->ejecuta($qry2);
+    echo "eliminado correctamente";
 }else{
-    echo "No haz escrito el id";
+    
+?>
+<form action="eliminar-productos.php" method="POST">
+    <div class="row">
+        <div class="form-grupo">
+            <label for="">Escribe el id</label>
+            <input type="text" value="" name="id" id="id" placeholder="escribe aqui el id">
+        </div>
+        <div class="form-group">
+            <button type="submit" name="eliminar" value="eliminar" class="btn btn-danger">Eliminar</button>
+        </div>
+    </div>
+</form>
+<?php
 }
-
 ?>
